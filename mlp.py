ce4e90cbb5b8b54b7b1c2a9cdd84c81062fec6b8
guilhermesantos <guilherme.milan9@gmail.com>
2019-03-21 23:49:24 -0300

    codigo do backpropagation

--- mlp.py
+++ mlp.py
@@ -1,6 +1,7 @@
 import numpy as np
 import imageio
 import math
+import functools
 
 #Classe que representa o multilayer perceptron
 class MLP():
@@ -15,6 +16,9 @@ class MLP():
 	def activ(self, net):
 		return (1/(1+math.exp(net)))
 
+	def deriv_activ(self, net):
+		return net*(1-net)
+
 	#Faz inferencia
 	def forward(self, input_vect):
 		if(input_vect.shape[0] != self.input_length):
@@ -25,29 +29,54 @@ class MLP():
 		biased_input[input_vect.shape[0]] = 1
 
 		hidden_net = np.dot(self.hidden_mat, biased_input)
-		hidden_activ = [self.activ(x) for x in hidden_net]
+		hidden_fnet = [self.activ(x) for x in hidden_net]
 
 		biased_hidden_activ = np.zeros((self.hidden_length+1))
-		biased_hidden_activ[0:self.hidden_length] = hidden_activ[:]
+		biased_hidden_activ[0:self.hidden_length] = hidden_fnet[:]
 		biased_hidden_activ[self.hidden_length] = 1
 		out_net = np.dot(self.output_mat, biased_hidden_activ)
-		out_activ = [self.activ(x) for x in out_net]
+		out_fnet = [self.activ(x) for x in out_net]
 
-		return out_activ
+		return hidden_net, hidden_fnet, out_net, out_fnet
+
+
 
 	#Faz backpropagation
-	def fit(self, input_samples, expected_labels, learning_rate, threshold):
-		
-		error = 0
-		while(error > threshold):
-			error = 0
+	def fit(self, input_samples, target_labels, learning_rate, threshold):
+		squared_error = 2*threshold
+		while(squared_error > threshold):
+			squared_error = 0
+			for i in range(0, input_samples.shape[0]):
+				input_sample = input_samples[i]
+				input_sample_with_bias = np.zeros(input_sample.shape)
+				input_sample_with_bias[0:input_sample.shape[0]] = input_sample[:]
+				input_sample_with_bias[input_sample.shape[0]]
 
+				hidden_net, hidden_fnet, out_net, out_fnet = self.forward(input_samples[i])
+				target_label = target_labels[i]
+				error_array = (target_label - out_fnet)
+				squared_error = squared_error + np.sum(error_array**2)
+
+				delta_o = error * deriv_activ(out_fnet)
+				output_weights = self.output_mat[:,0:hidden_length]
+				delta_h = deriv_activ(hidden_fnet) * np.dot(delta_o, output_weights)
+
+				self.output_mat = self.output_mat + learning_rate*np.dot(delta_o, hidden_fnet)
+				self.hidden_mat = self.hidden_mat + learning_rate*np.dot(delta_h.T, input_sample_with_bias)
+			squared_error = squared_error/input_samples.shape[0]
+			print('Erro medio quadratico', squared_error)
 		return None
+
+def squared_error(target, output):
+	squared_differences = [(target[x] - output[x])**2 for x in range(len(target))]
+	return functools.reduce(lambda x,y: x+y, squared_differences)
 
 def main():
 	mlp = MLP(*(2, 2, 2))
 	out = mlp.forward(np.random.uniform(-1., 1., (2)))
 	print('output', out)
+	vec1 = [1, 0, 0, 1]
+	vec2 = [1, 0, 0, -1]
 
 if __name__ == '__main__':
 	main()
